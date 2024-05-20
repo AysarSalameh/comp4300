d9f14dd3ef070dc6ad57e0571d5a91f7bb39a57a
AysarSalameh <132439155+AysarSalameh@users.noreply.github.com>
2024-05-20 12:12:13 +0300

    last modification

--- COMP43001/script.js
+++ COMP43001/script.js
@@ -1,7 +1,12 @@
-
 document.getElementById('all').addEventListener('change', function() {
-  var checkboxes = document.querySelectorAll('.rights input[type="checkbox"]');
-  checkboxes.forEach(function(checkbox) {
-    checkbox.checked = this.checked;
-  }.bind(this));
-});
+    var checkboxes = document.querySelectorAll('.rights input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = this.checked;
+    }.bind(this));
+  });
+  
+  var evaluateBtn = document.getElementById('evaluate-btn');
+  var centerDiv = document.querySelector('.center-container');
+  evaluateBtn.addEventListener('click', function() {
+    centerDiv.classList.toggle('hidden');
+  });
